de4a2a71c112af15f3afab4ffb73ad054c781479
Alan Falcon <falconswoosh@gmail.com>
2017-09-22 16:34:32 -0700

    fix first logic again.

--- js/scripts.js
+++ js/scripts.js
@@ -3,43 +3,42 @@
   var exceptionDivisors = [15,5,3];
   var exceptionTexts = ["ping-pong","pong","ping"];
 //Accept only positive integers. Return positve integer greater than zero
-var positiveInteger = function(number){
-  if (number <= 0){
+var positiveInteger = function(number) {
+  if (number <= 0) {
     return "Try again.  Please enter an integer greater than 0."
   }
 };
-//Create new array from 1 to the user input number.
-var tempArray = function(number) {
-  for (var i=1; i<=number; i++) {
-    tempArray.push(i);
-  }
-  return tempArray;
-//
-};
-//TODO: test for blanks
-// var transformedOutput = function(input) {
-//   var exceptionDivisors = [15,5,3];
-//   var exceptionTexts = ["ping-pong","pong","ping"];
-//   // var numberToDecrement = input;
-//
-//   for (index=0; index < numberToDecrement; index--) {
-//     if ((input.length > 1) && (input % exceptionDivisors[index] === 0)) {
-//       var newNumber = numberToDecrement--;
-//       alert("input.length: " + input.)
-//       alert("newNumber: " + newNumber);
-//       return exceptionTexts[index];
-//     } else if ((input.length = 1) && (input % exceptionDivisors[index] === 0)) {
-//       var newNumber = numberToDecrement--;
-//       return exceptionTexts[index];
-//     } else if (input % exceptionDivisors[index] === 0) {
-//       var newNumber = numberToDecrement--;
-//       return exceptionTexts[index];
-//     } else {
-//       var newNumber = numberToDecrement--;
-//       return newNumber;
-//     }
+// //Create new array from 1 to the user input number.
+// var outputArray = function(number) {
+//   for (var index=1; index<=number; index++) {
+//     outputArray.push(index);
 //   }
-// }
+//   return outputArray;
+// //
+// };
+//TODO: test for blanks
+var transformedOutput = function(input) {
+  var exceptionDivisors = [15,5,3];
+  var exceptionTexts = ["ping-pong","pong","ping"];
+  var temp = []
+  var number = input;
+
+  for (index=0; index < number; index--) {
+    if ((input.length > 1) && (input % exceptionDivisors[index] === 0)) {
+      var newNumber = number--;
+      return exceptionTexts[index];
+    // } else if ((input.length = 1) && (input % exceptionDivisors[index] === 0)) {
+    //   var newNumber = numberToDecrement--;
+    //   return exceptionTexts[index];
+    // } else if (input % exceptionDivisors[index] === 0) {
+    //   var newNumber = numberToDecrement--;
+    //   return exceptionTexts[index];
+    // } else {
+    //   var newNumber = numberToDecrement--;
+      // return newNumber;
+    }
+  }
+}
 
 
 
@@ -50,11 +49,9 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     var number = $("#number").val();
-    // var result = transformedOutput(input);
-    $("#output").text(""); //Clear output section
+    var result = transformedOutput(number);
+    // $("#output").text(""); //Clear output section
     $("#output").show();
-      for (var i = 0; i < outputArray.length; i++) {
-        $("#output").prepend("<li>"+outputArray[i]+"</li>");
-      }
+    $("#output").prepend("<li>"+result+"</li>");
     });
 });
