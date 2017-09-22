86e19b6496b7b4f430c7b650932cb8cba8ff5931
Alan Falcon <falconswoosh@gmail.com>
2017-09-22 12:14:06 -0700

    fix transposition of index and input.length in for loop.

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,9 @@ var exceptionTexts = ["ping","pong","ping-pong"];
 
 //TODO: test for blanks
 var transformedOutput = function(input) {
-  for (index=0; input.length < index; index++);
-    if (input===17) {
-      return alert(input.length);
+  for (index=0; index <= input.length; index++);
+    if (input==="17") {
+      return alert(index);
     }
 }
 
