9aefbe041853a95d9aa8f9ca4f4207d4a7ed34e6
Alan Falcon <falconswoosh@gmail.com>
2017-09-22 13:51:12 -0700

    attempt to add feature to decrement from input value to display input value and each value in between to 1.

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,19 @@
 
 //TODO: test for blanks
 var transformedOutput = function(input) {
-  var exceptionDivisors = [3,5,15];
-  var exceptionTexts = ["ping","pong","ping-pong"];
-  for (index=0; index < input.length; index++) {
-    if (input % exceptionDivisors[index] === 0) {
+  var exceptionDivisors = [15,5,3];
+  var exceptionTexts = ["ping-pong","pong","ping"];
+  var numberToDecrement = input
+  for (index=0; index < numberToDecrement; index--) {
+    if ((input.length > 1) && (input % exceptionDivisors[index] === 0)) {
+      var newNumber = numberToDecrement--
       return exceptionTexts[index];
     } else if ((input.length > 1) && (input % exceptionDivisors[index] === 0)) {
       return exceptionTexts[index];
     } else if (input % exceptionDivisors[index] === 0) {
       return exceptionTexts[index];
+    } else {
+      return
     }
   }
 }
