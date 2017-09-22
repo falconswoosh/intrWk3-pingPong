664062972175646887bf6bfb7645210fd6d42409
Alan Falcon <falconswoosh@gmail.com>
2017-09-22 12:09:54 -0700

    fix input to be a string value by removal of ParseInit().

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 //Business Interface
 var exceptionDivisors = [3,5,15];
 var exceptionTexts = ["ping","pong","ping-pong"];
+
+//TODO: test for blanks
 var transformedOutput = function(input) {
-  for(index=0; input.length < index; index++);
-  return alert(transformedOutput);
+  for (index=0; input.length < index; index++);
+    if (input===17) {
+      return alert(input.length);
+    }
 }
 
 
@@ -17,9 +21,9 @@ var transformedOutput = function(input) {
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    // var inputParseInit = input.parseInit();
-    // var inputVal = $("input#number").val();
-    var input = parseInt($("#number").val());
+    // var input = parseInt($("#number").val());
+    // BI: alert wasn't working for input.length due to ParseInit;
+    var input = $("#number").val();
     var result = transformedOutput(input);
     $("#output").text(result);
     $("#output").show();
