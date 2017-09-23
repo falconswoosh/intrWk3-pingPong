1c934b746c858680dda8473ccd1caafed409b351
Alan Falcon <falconswoosh@gmail.com>
2017-09-22 17:36:56 -0700

    logic outputting undefined!

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,32 @@
 //Business Interface
-//Global variables
-  var exceptionDivisors = [15,5,3];
-  var exceptionTexts = ["ping-pong","pong","ping"];
-//Accept only positive integers. Return positve integer greater than zero
-var positiveInteger = function(number) {
-  if (number <= 0) {
-    return "Try again.  Please enter an integer greater than 0."
-  }
-};
-// //Create new array from 1 to the user input number.
-// var outputArray = function(number) {
-//   for (var index=1; index<=number; index++) {
-//     outputArray.push(index);
-//   }
-//   return outputArray;
-// //
-// };
-//TODO: test for blanks
-var transformedOutput = function(input) {
-  var exceptionDivisors = [15,5,3];
-  var exceptionTexts = ["ping-pong","pong","ping"];
-  var temp = []
-  var number = input;
 
-  for (index=0; index < number; index--) {
-    if ((input.length > 1) && (input % exceptionDivisors[index] === 0)) {
-      var newNumber = number--;
-      return exceptionTexts[index];
-    // } else if ((input.length = 1) && (input % exceptionDivisors[index] === 0)) {
-    //   var newNumber = numberToDecrement--;
-    //   return exceptionTexts[index];
-    // } else if (input % exceptionDivisors[index] === 0) {
-    //   var newNumber = numberToDecrement--;
-    //   return exceptionTexts[index];
-    // } else {
-    //   var newNumber = numberToDecrement--;
-      // return newNumber;
+var transformedOutput = function(number) {
+  var exceptDivisors = [15,5,3];
+  var exceptTxts = ["ping-pong","pong","ping"];
+  var exceptIndex = 0;
+  var outputArray = [];
+alert("start")
+alert(number)
+  for (index=number; index < number; index--) {
+    if ((number.length > 1) && (number % exceptDivisors[exceptIndex] === 0)) {
+        outputArray.push(exceptTxts[exceptIndex]);
+        exceptIndex = exceptIndex++
+        alert("Logic1 this is the outputArray: " + outputArray + "eIndex: " + exceptIndex);
+        alert("Txt: " + exceptTxts[exceptIndex]);
+        // return exceptTxts[exceptIndex]; //temporary test
+      } else if (number % exceptDivisors[exceptIndex] === 0) {
+        outputArray.push(exceptTxts[exceptIndex]);
+        exceptIndex = exceptIndex++
+        alert("Logic2 this is the outputArray: " + outputArray + "eIndex: " + exceptIndex);
+        alert("Txt: " + exceptTxts[exceptIndex]);
+        // return exceptTxts[exceptIndex]; //temporary test
+      // } else if (input % exceptDivisors[index] === 0) {
+      //   var newNumber = numberToDecrement--;
+      //   return exceptTxts[index];
+      // } else {
+      //   var newNumber = numberToDecrement--;
+        // return newNumber;
+alert("end");
     }
   }
 }
@@ -52,6 +43,7 @@ $(document).ready(function() {
     var result = transformedOutput(number);
     // $("#output").text(""); //Clear output section
     $("#output").show();
-    $("#output").prepend("<li>"+result+"</li>");
+    $("#list").prepend("<li>"+result+"</li>");
+
     });
 });
