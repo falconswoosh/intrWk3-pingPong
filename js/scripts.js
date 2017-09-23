6a84fa8b3ce3cf8ac057c1bad618ab00ea4cd3b3
Alan Falcon <falconswoosh@gmail.com>
2017-09-22 19:49:19 -0700

    fix output; yet still broken logic where not displaying decrements of numbers, ping, pong, and ping-pong

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,47 @@
 //Business Interface
-
+var exceptDivisors = [15,5,3];
+var exceptTxts = ["ping-pong","pong","ping"];
+var exceptIndex = 0;
+// var newNumber = number;
+var outputArray = [];
 var transformedOutput = function(number) {
-  var exceptDivisors = [15,5,3];
-  var exceptTxts = ["ping-pong","pong","ping"];
-  var exceptIndex = 0;
-  var newNumber = 0
-  var outputArray = [];
+  // var exceptDivisors = [15,5,3];
+  // var exceptTxts = ["ping-pong","pong","ping"];
+  // var exceptIndex = 0;
+  var newNumber = number;
+  // var outputArray = [];
 alert("start");
 alert("Input number is: " + number);
-  for (index=newNumber; index <= newNumber; index--) {
-    if ((number.length > 1) && (number % exceptDivisors[exceptIndex] === 0)) {
+alert("number length: " + number.length);
+  for (var index=0; index <= number; index++) {
+    if ((number.length >= 2) && (exceptIndex < 3) && (newNumber != 0) && (number % exceptDivisors[exceptIndex] === 0)) {
+        alert("Logic1");
         outputArray.push(exceptTxts[exceptIndex]);
-        exceptIndex = exceptIndex++
-        newNumber = newNumber--
+        exceptIndex = exceptIndex++;
+        newNumber = newNumber--;
         alert("Logic1 this is the outputArray: " + outputArray + " eIndex: " + exceptIndex);
         alert("Txt: " + exceptTxts[exceptIndex]);
-      } else if (exceptIndex === 3) {
-        exceptIndex = 0
         // return exceptTxts[exceptIndex]; //temporary test
-      } else if (number % exceptDivisors[exceptIndex] === 0) {
+      } else if ((exceptIndex < 3) && (newNumber > 0) && (number % exceptDivisors[exceptIndex] === 0)) {
         outputArray.push(exceptTxts[exceptIndex]);
-        exceptIndex = exceptIndex++
-        newNumber = newNumber--
+        exceptIndex = exceptIndex++;
+        newNumber = newNumber--;
         alert("Logic2 this is the outputArray: " + outputArray + " eIndex: " + exceptIndex);
         alert("Txt: " + exceptTxts[exceptIndex]);
-      } else if (exceptIndex === 3) {
-        exceptIndex = 0
+      } else if ((exceptIndex < 3) && newNumber > 0) {
         outputArray.push(newNumber);
-        // return exceptTxts[exceptIndex]; //temporary test
-      // } else if (input % exceptDivisors[index] === 0) {
-      //   var newNumber = numberToDecrement--;
-      //   return exceptTxts[index];
-      // } else {
-      //   var newNumber = numberToDecrement--;
-        // return newNumber;
+        exceptIndex = 0;
+        newNumber = newNumber--;
+      } else if (newNumber === 0) {
+        alert("Logic2 this is the outputArray: " + outputArray + " eIndex: " + exceptIndex);
+        return outputArray;
 alert("end");
     }
   }
 }
+
+
+
 
 
 
@@ -49,9 +53,11 @@ $(document).ready(function() {
     event.preventDefault();
     var number = $("#number").val();
     var result = transformedOutput(number);
+    var cloneArray = outputArray.slice();
     // $("#output").text(""); //Clear output section
-    $("#output").show();
-    $("#list").prepend("<li>"+result+"</li>");
-
-    });
+		for (var i=0; i<cloneArray.length;i++) {
+      $("#output").show();
+  		$("#list").prepend("<li>"+cloneArray[i]+"</li>");
+    }
+  });
 });
